225f1071cf4935bb22f4ae201724818846ea84bf
Sanket Kadam <sk8185710@gmail.com>
2026-03-05 12:45:53 +0530

    update home route message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello CI/CD AND WORKING CORRECTLY"
+    return "Hello CI/CD AND WORKING CORRECTLY and hello sanket "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
