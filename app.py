91b197926c5ba908b07976a23ac2d0faf3ed7984
Sanket Kadam <sk8185710@gmail.com>
2026-03-05 12:30:28 +0530

    test cicd auto deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello CI/CD"
+    return "Hello CI/CD AND WORKING CORRECTLY"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
